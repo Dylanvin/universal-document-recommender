efdaa3741147263bb0959a38cac5801684fcb7c7
Dylanvin <dylanvinc@hotmail.co.uk>
2021-02-11 19:15:50 +0000

    optimised tf method added

--- document_methods.py
+++ document_methods.py
@@ -30,6 +30,21 @@ class DocumentSimilarity:
                 tf[word][query_index] = words.count(word)/len(words)
         return tf
 
+    def tfAndQuery(self, df, colnames, doc, catagory):
+        tf = {}
+        df = df.append({colnames[0]:catagory, colnames[1]:doc}, ignore_index=True)
+        query_words = set(re.sub("[^\w]", " ", doc).split())
+        for index, row in df.iterrows():
+            words = re.sub("[^\w]", " ", row[colnames[1]]).split()
+            for word in set(words):
+                if word in query_words:
+                    if word in tf:
+                        tf[word][index] = words.count(word)/len(words)
+                    else:
+                        tf[word] = [0] * (len(df.index))
+                        tf[word][index] = words.count(word)/len(words)
+        return tf
+
     #IDF section (run after request) only finds idf of query doc words
     def queryIdf(self, df, colnames, doc):
         idf = {}
@@ -44,12 +59,12 @@ class DocumentSimilarity:
                         idf[word] = 1
 
         for key in idf:
-            idf[key] = math.log10(float((len(df.index) + 1)/(idf[key] + 1.0)))
+            idf[key] = math.log10(float((len(df.index))/(idf[key] + 1.0)))
 
         return idf
 
-    def tdfIdf(self, tf, df, colnames, query_doc):
-        qtf = self.queryTf(df, tf, query_doc)
+    def tdfIdf(self, df, colnames, query_doc, catagory):
+        qtf = self.tfAndQuery(df, colnames, query_doc, catagory)
         qidf = self.queryIdf(df, colnames, query_doc)
         tf_idf = {}
         for key in qidf:
--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import pandas as pd
 colnames=['Catagory', 'Text']
 df = pd.read_csv('20ng-test-all-terms.txt', names = colnames, sep = '\t')
 query_doc = "data science is the sexiest job of the 21st century"
+catagory = 'science'
 
 ds = DocumentSimilarity()
-tf = ds.tf(df, colnames)
+#tf = ds.tf(df, colnames)
 print("tf done")
 
 
 #call this on request
-qtf = dict(tf)
-tf_idf = ds.tdfIdf(tf, df, colnames, query_doc)
+#qtf = dict(tf)
+tf_idf = ds.tdfIdf(df, colnames, query_doc, catagory)
 print("tf_idf done")
-#print(tf_idf)
+print(tf_idf)
